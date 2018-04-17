25a526e68bc6a34a87c2945103dc52a6f8280ff4
Huascar <huascarmc@hotmail.com>
2018-04-17 20:08:53 +0100

    Added display selection

--- src/Containers/DisplayContainer/DisplayContainer.js
+++ src/Containers/DisplayContainer/DisplayContainer.js
@@ -3,22 +3,33 @@ import React from 'react';
 class DisplayContainer extends React.Component {
  constructor(props) {
   super(props);
+
+  this.state = {
+    selection: "a"
+  }
 }
 // Updates the number property in state when input value changes.
-
+setSelection(choice) {
+  this.setState({
+    selection: choice
+  })
+}
 
  render() {
  // Renders a form to get user input and passes the value of state to props table container.
 
   return(
+    <div>
+  <div className="choice">{this.state.selection}</div>
   <div className="display">
     <ul>
-      <li><p className="price">$0.65<button className="coke" onClick={this.props.setSelection("coke")} >COKE</button></p></li>
-      <li><p className="price">$1.00<button className="pepsi" onClick={this.props.setSelection("pepsi")}>PEPSI</button></p></li>
-      <li><p className="price">$1.50<button className="soda" onClick={this.props.setSelection("soda")}>SODA</button></p></li>
-      <li><p className="price">$2.00<button className="water" onClick={this.props.setSelection("water")}>WATER</button></p></li>
+      <li><p className="price">$0.65<button className="coke" onClick={() => this.setSelection("coke") } >COKE</button></p></li>
+      <li><p className="price">$1.00<button className="pepsi" onClick={() => this.setSelection("pepsi") }>PEPSI</button></p></li>
+      <li><p className="price">$1.50<button className="soda" onClick={() => this.setSelection("soda") }>SODA</button></p></li>
+      <li><p className="price">$2.00<button className="water" onClick={() =>this.setSelection("water") }>WATER</button></p></li>
     </ul>
  </div>
+</div>
    );
   }
  }
--- src/Containers/Machine/MachineContainer.js
+++ src/Containers/Machine/MachineContainer.js
@@ -5,15 +5,9 @@ import DisplayContainer from '../DisplayContainer/DisplayContainer.js'
 class MachineContainer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      selection: "a",
-    }
 
 
-  }
 
-  setSelection(selection) {
-    this.state.selection = selection;
   }
 
 
@@ -21,8 +15,7 @@ class MachineContainer extends React.Component {
     return(
       <div className="machine">
           <InputContainer />
-          <div className="choice">{this.state.selection}</div>
-          <DisplayContainer setSelection={this.setSelection.bind(this)}/>
+          <DisplayContainer />
       </div>
     )
   }
