30522d4e4a0c0e35370463437f6cc9bf2d8a73cf
Huascar <huascarmc@hotmail.com>
2018-04-18 08:57:02 +0100

    bucket container renders change succesfully

--- src/Containers/BucketContainer/BucketContainer.js
+++ src/Containers/BucketContainer/BucketContainer.js
@@ -2,25 +2,25 @@ import React from 'react';
 
 class BucketContainer extends React.Component {
 
-  // generateChange() {
-  //   var htmlChange = []
-  //   for(var i = 0; i < this.props.change.length; i++) {
-  //     htmlChange.unshift(this.generateCoin(this.props.change[i]))
-  //     // console.log(this.props.change[i])
-  //   }
-  //   return htmlChange
-  // }
+  generateChange() {
+    var htmlChange = []
+    for(var i = 0; i < this.props.change.length; i++) {
+      htmlChange.unshift(this.generateCoin(this.props.change[i]))
+      // console.log(this.props.change[i])
+    }
+    return htmlChange
+  }
 
   generateCoin(coin) {
     return <li><p className="coinValue">{coin}<button className="coin bronze" onClick={ () => this.updateBalance(0.01) }></button></p></li>
   }
 
  render() {
-   console.log(this.props.change)
+  var result = this.generateChange()
    // var result = this.generateChange()
   return(
   <div className="bucket">
-    { this.props.change }
+    { result }
  </div>
    );
   }
