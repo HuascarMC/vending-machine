fdbfee98428efadd14b58c53ec86122ec032f9cd
Huascar <huascarmc@hotmail.com>
2018-04-18 10:57:06 +0100

    transaction state renders succesfully

--- src/Containers/BucketContainer/BucketContainer.js
+++ src/Containers/BucketContainer/BucketContainer.js
@@ -12,43 +12,46 @@ class BucketContainer extends React.Component {
       // console.log(this.props.change[i])
     }
     return htmlChange
-    }
   }
+}
 
-  generateCoin(coin) {
-    switch(coin) {
-     case "PENNY":
-       return <li><p className="coinValue change">{coin.toString()}      $0.01<button className="coin bronze" onClick={ () => this.updateBalance(0.01) }></button></p></li>
-     case "NICKEL":
-       return <li><p className="coinValue change">{coin.toString()}      $0.01<button className="coin bronze" onClick={ () => this.updateBalance(0.01) }></button></p></li>
-     case "DIME":
-       return <li><p className="coinValue change">{coin.toString()}      $0.05<button className="coin silver" onClick={ () => this.updateBalance(0.05) }></button></p></li>
-     case "QUARTER":
-       return <li><p className="coinValue change">{coin.toString()}      $1.00<button className="coin gold" onClick={ () => this.updateBalance(1.00) }></button></p></li>
-     case "DOLLAR":
-      return <li><p className="coinValue change">{coin.toString()}       $1.00<button className="coin gold" onClick={ () => this.updateBalance(1.00) }></button></p></li>
-   }
+generateCoin(coin) {
+  switch(coin) {
+    case "PENNY":
+    return <li><p className="coinValue change">{coin.toString()}      $0.01<button className="coin bronze" onClick={ () => this.updateBalance(0.01) }></button></p></li>
+    case "NICKEL":
+    return <li><p className="coinValue change">{coin.toString()}      $0.01<button className="coin bronze" onClick={ () => this.updateBalance(0.01) }></button></p></li>
+    case "DIME":
+    return <li><p className="coinValue change">{coin.toString()}      $0.05<button className="coin silver" onClick={ () => this.updateBalance(0.05) }></button></p></li>
+    case "QUARTER":
+    return <li><p className="coinValue change">{coin.toString()}      $1.00<button className="coin gold" onClick={ () => this.updateBalance(1.00) }></button></p></li>
+    case "DOLLAR":
+    return <li><p className="coinValue change">{coin.toString()}       $1.00<button className="coin gold" onClick={ () => this.updateBalance(1.00) }></button></p></li>
   }
+}
 
- render() {
+render() {
   var result = this.generateChange()
-   // var result = this.generateChange()
-   console.log(this.props.state.stockInventoryState);
+  // var result = this.generateChange()
+  console.log(this.props.state.stockInventoryState);
   return(
-  <div>
-    <p className="title-total-change">Change Total: </p>
-    <div className="changeTotal">
-          <p className="balance-text">${ this.props.changeTotal.toFixed(2) }</p>
+    <div>
+      <p className="title-total-change">Change Total: </p>
+      <div className="changeTotal">
+        <p className="balance-text">${ this.props.changeTotal.toFixed(2) }</p>
+      </div>
+      <div className="bucket">
+        { result }
+      </div>
+      <div className="transaction-state-container">
+        <p className="title-transaction-state">Transaction State: </p>
+        <div className="transaction-state">
+          <p className="state-title">In Stock: {JSON.stringify(this.props.state.stockInventoryState) } </p>
+          <p className="state-title">Enough change: {JSON.stringify(this.props.state.coinInventoryState) } </p>
+          <p className="state-title">Enough balance: {JSON.stringify(this.props.state.balanceState) } </p>
+        </div>
+      </div>
     </div>
-  <div className="bucket">
-    { result }
- </div>
-<div>
-  <p className="state-title">In Stock: {JSON.stringify(this.props.state.stockInventoryState) } </p>
-  <p className="state-title">Enough change: {JSON.stringify(this.props.state.coinInventoryState) } </p>
-  <p className="state-title">Enough balance: {JSON.stringify(this.props.state.balanceState) } </p>
-</div>
-</div>
    );
   }
  }
