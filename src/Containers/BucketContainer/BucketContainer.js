8fdb098eaf87628907e34d19305f1bc768210e1d
Huascar <huascarmc@hotmail.com>
2018-04-19 15:12:24 +0100

    changed rendered value

--- src/Containers/BucketContainer/BucketContainer.js
+++ src/Containers/BucketContainer/BucketContainer.js
@@ -28,9 +28,9 @@ generateCoin(coin) {
     case "PENNY":
     return <li><p className="coinValue change">{coin.toString()}      $0.01<button className="coin bronze" onClick={ () => this.updateBalance(0.01) }></button></p></li>
     case "NICKEL":
-    return <li><p className="coinValue change">{coin.toString()}      $0.01<button className="coin bronze" onClick={ () => this.updateBalance(0.01) }></button></p></li>
+    return <li><p className="coinValue change">{coin.toString()}      $0.05<button className="coin bronze" onClick={ () => this.updateBalance(0.01) }></button></p></li>
     case "DIME":
-    return <li><p className="coinValue change">{coin.toString()}      $0.05<button className="coin silver" onClick={ () => this.updateBalance(0.05) }></button></p></li>
+    return <li><p className="coinValue change">{coin.toString()}      $0.10<button className="coin silver" onClick={ () => this.updateBalance(0.05) }></button></p></li>
     case "QUARTER":
     return <li><p className="coinValue change">{coin.toString()}      $0.25<button className="coin gold" onClick={ () => this.updateBalance(1.00) }></button></p></li>
     case "DOLLAR":
