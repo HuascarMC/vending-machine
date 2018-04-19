2307d92e3bb3613690ca7ab1ba090cf2f263eece
Huascar <huascarmc@hotmail.com>
2018-04-20 00:49:11 +0100

    refactor displaycontainer

--- src/Containers/DisplayContainer/DisplayContainer.js
+++ src/Containers/DisplayContainer/DisplayContainer.js
@@ -1,40 +1,18 @@
 import React from 'react';
 
 class DisplayContainer extends React.Component {
- constructor(props) {
-  super(props);
-
-  this.state = {
-    selection: ""
+  render() {
+    return(
+      <div className="display">
+        <ul>
+          <li><p className="price">$0.65<button className="coke" onClick={() => this.props.updateItem("coke") } >COKE</button></p></li>
+          <li><p className="price">$1.00<button className="pepsi" onClick={() => this.props.updateItem("pepsi") }>PEPSI</button></p></li>
+          <li><p className="price">$1.50<button className="soda" onClick={() => this.props.updateItem("soda") }>SODA</button></p></li>
+          <li><p className="price">$2.00<button className="water" onClick={() =>this.props.updateItem("water") }>WATER</button></p></li>
+        </ul>
+      </div>
+    );
   }
-
 }
-
-setSelection(choice) {
-  this.setState({
-    selection: choice
-  })
-  this.updateItem(choice)
-}
-
-updateItem(choice) {
-  this.props.updateItem(choice)
-}
-
- render() {
-
-  return(
-
-  <div className="display">
-    <ul>
-      <li><p className="price">$0.65<button className="coke" onClick={() => this.setSelection("coke") } >COKE</button></p></li>
-      <li><p className="price">$1.00<button className="pepsi" onClick={() => this.setSelection("pepsi") }>PEPSI</button></p></li>
-      <li><p className="price">$1.50<button className="soda" onClick={() => this.setSelection("soda") }>SODA</button></p></li>
-      <li><p className="price">$2.00<button className="water" onClick={() =>this.setSelection("water") }>WATER</button></p></li>
-    </ul>
- </div>
-   );
-  }
- }
 
 export default DisplayContainer;
