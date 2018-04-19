41c9309fe78fb177629103f32a94dc5d3e838a4a
Huascar <huascarmc@hotmail.com>
2018-04-19 19:49:53 +0100

    sets image to undefined if order not succesful

--- src/App.js
+++ src/App.js
@@ -44,8 +44,8 @@ setImage() {
       case "WATER":
       return this.setimg("http://www.pngall.com/wp-content/uploads/2016/04/Water-Bottle-PNG-Picture.png")
     }
-    this.setimg(0)
   }
+  this.setimg(undefined)
 }
 
 
