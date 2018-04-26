4e34bf6a2739411c667fc0a1f57cc87c3c2b74c9
Huascar <huascarmc@hotmail.com>
2018-04-26 15:42:48 +0100

    Added body container

---
+++ src/Containers/BodyContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class BodyContainer extends React.Component {
+
+
+  render() {
+    return(
+      <div className="content-wrapper">
+      </div>
+    );
+  }
+}
+
+export default BodyContainer;
