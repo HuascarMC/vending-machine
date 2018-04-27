92b48c3be39faab56f2c43bfdb7dee48a8046a62
Huascar <huascarmc@hotmail.com>
2018-04-27 17:09:40 +0100

    added increase quantity method to coin controller

--- src/Components/CoinController.js
+++ src/Components/CoinController.js
@@ -2,10 +2,21 @@ import React from 'react';
 
 class CoinController extends React.Component {
   render() {
+
+    increaseCoinQuantity(coin) {
+      axios.put(`https://vending-machine-server.herokuapp.com/machine/addcoin/${coin}`, { crossdomain: true})
+      .then((response) => {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+    }
+
     return(
       <div className="controller-wrapper">
         <ul>
-          <li>COKE<button className="plus">+</button><button className="minus">-</button></li>
+          <li>COKE<button onClick={() => this.increaseCoinQuantity()} className="plus">+</button><button className="minus">-</button></li>
           <li>PEPSI<button className="plus">+</button><button className="minus">-</button></li>
           <li>SODA<button className="plus">+</button><button className="minus">-</button></li>
           <li>WATER<button className="plus">+</button><button className="minus">-</button></li>
