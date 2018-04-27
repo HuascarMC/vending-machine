2c52ba3d00c77af9d6406a7a46f5af310cc6cdfe
Huascar <huascarmc@hotmail.com>
2018-04-27 20:53:13 +0100

    added update balance function

--- src/Containers/MachineContainer.js
+++ src/Containers/MachineContainer.js
@@ -4,7 +4,20 @@ import InputComponent from '../Components/InputComponent'
 import OrderButtonComponent from '../Components/OrderButtonComponent'
 
 class MachineContainer extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      item: 0.00,
+      balance: 0.00
+    }
+  }
+
+  updateBalance(amount) {
+    this.setState({
+      balance: this.state.balance + amount
+    })
+  }
 
   render() {
     return(
