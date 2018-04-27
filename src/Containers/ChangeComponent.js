781a8bbacc8a31463dcba270df68552d719250f8
Huascar <huascarmc@hotmail.com>
2018-04-27 10:01:19 +0100

    created change component

---
+++ src/Containers/ChangeComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class ChangeContainer extends React.Component {
+
+
+  render() {
+    return(
+      <div className="change-wrapper">
+        
+      </div>
+    );
+  }
+}
+
+export default ChangeContainer;
