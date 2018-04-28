a7611ace011c3ef140e9980b83acbaec204ef909
Huascar <huascarmc@hotmail.com>
2018-04-28 21:31:46 +0100

    state of transaction info renders well

--- src/Components/StateComponent.js
+++ src/Components/StateComponent.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 class StateComponent extends React.Component {
   render() {
+    console.log(this.props.state);
     return(
       <div className="state-wrapper">
         <p className="title">state</p>
-          <p className="state-title">In Stock:</p>
-          <p className="state-title">Enough change:</p>
-          <p className="state-title">Enough balance:</p>
+        <p className="state-title">In Stock: {JSON.stringify(this.props.state.transactionState.stockInventoryState) } </p>
+        <p className="state-title">Enough change: {JSON.stringify(this.props.state.transactionState.coinInventoryState) } </p>
+        <p className="state-title">Enough balance: {JSON.stringify(this.props.state.transactionState.balanceState) } </p>
       </div>
     );
   }
