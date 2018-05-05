247694804a134ffd630b820cca9d0e4f9f65f804
Huascar <huascarmc@hotmail.com>
2018-05-05 17:37:05 +0100

    should render without crashing test passeD

--- src/tests/Components/CoinController.test.js
+++ src/tests/Components/CoinController.test.js
@@ -2,3 +2,15 @@ import React from 'react';
 import CoinController from '../../Components/CoinController'
 import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
+
+var state = {
+  dollar: 0,
+  quarter: 0,
+  dime: 0,
+  nickel: 0,
+  penny: 0,
+}
+
+it('should render without crashing', () => {
+  shallow(<CoinController coins={state}/>);
+});
