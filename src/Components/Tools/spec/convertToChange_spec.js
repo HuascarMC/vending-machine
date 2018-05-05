4e5fe939833d62ebcabc6aefee313b5e7690aa5a
Huascar <huascarmc@hotmail.com>
2018-05-05 17:15:07 +0100

    refactored ConvertToChange tool test

--- src/Components/Tools/spec/convertToChange_spec.js
+++ src/Components/Tools/spec/convertToChange_spec.js
@@ -1,51 +1,42 @@
 var assert = require('assert');
-var convertToChange = require('../convertToChange');
+var ConvertToChange = require('../ConvertToChange');
 
-describe('convertToChange', function () {
+describe('ConvertToChange', function () {
 
 
  it('it converts change dollar', function () {
-  const actualResult = convertToChange(1)
+  const actualResult = ConvertToChange(1)
   const expectedResult = ["DOLLAR"]
-  console.log(actualResult);
   assert.deepEqual(actualResult, expectedResult)
  });
 
  it('it converts change quarter', function () {
-  const actualResult = convertToChange(0.25)
+  const actualResult = ConvertToChange(0.25)
   const expectedResult = ["QUARTER"]
-  console.log(actualResult);
   assert.deepEqual(actualResult, expectedResult)
  });
 
  it('it converts change dime', function () {
-  const actualResult = convertToChange(0.1)
+  const actualResult = ConvertToChange(0.1)
   const expectedResult = ["DIME"]
-  console.log(actualResult);
   assert.deepEqual(actualResult, expectedResult)
  });
 
  it('it converts change nickel', function () {
-  const actualResult = convertToChange(0.05)
+  const actualResult = ConvertToChange(0.05)
   const expectedResult = ["NICKEL"]
-  console.log(actualResult);
   assert.deepEqual(actualResult, expectedResult)
  });
 
  it('it converts change penny', function () {
-  const actualResult = convertToChange(0.01)
+  const actualResult = ConvertToChange(0.01)
   const expectedResult = ["PENNY"]
-  console.log(actualResult);
   assert.deepEqual(actualResult, expectedResult)
  });
 
- it('it converts change penny', function () {
-  const actualResult = convertToChange(1.41)
+ it('it converts amount to multiple coins array', function () {
+  const actualResult = ConvertToChange(1.41)
   const expectedResult = ["DOLLAR", "QUARTER", "DIME", "NICKEL", "PENNY"]
-  console.log(actualResult);
   assert.deepEqual(actualResult, expectedResult)
  });
-
-
-
 });
