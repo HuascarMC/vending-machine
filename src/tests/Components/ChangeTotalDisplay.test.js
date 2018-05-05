fac3e2cd37203bef68706e93cccb2063ffff055b
Huascar <huascarmc@hotmail.com>
2018-05-05 17:23:09 +0100

    should render change-display div test passed

--- src/tests/Components/ChangeTotalDisplay.test.js
+++ src/tests/Components/ChangeTotalDisplay.test.js
@@ -7,6 +7,12 @@ it('should render without crashing', () => {
   shallow(<ChangeTotalDisplay amount={0.00}/>);
 });
 
+it('should render change-display div', () => {
+  const wrapper = shallow(<ChangeTotalDisplay amount={0.00}/>);
+  const div = wrapper.find('.change-display');
+  expect(div.exists()).toEqual(true);
+});
+
 it('should match the snapshot', () => {
   const component = renderer.create(<ChangeTotalDisplay amount={0.00}/>);
   let tree = component.toJSON();
