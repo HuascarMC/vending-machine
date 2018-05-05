d3224b1743266d3554226f2c09800d0bb02c3ad3
Huascar <huascarmc@hotmail.com>
2018-05-05 11:51:55 +0100

    should generate coin penny test passed

--- src/tests/Components/ChangeComponent.test.js
+++ src/tests/Components/ChangeComponent.test.js
@@ -6,3 +6,11 @@ import renderer from 'react-test-renderer';
 it('should render without crashing', () => {
   shallow(<ChangeComponent amount={0.00}/>);
 });
+
+it('should generate HTML coin PENNY', () => {
+  const wrapper = shallow(<ChangeComponent amount={0.00}/>);
+  const coin = 'PENNY';
+  const actual = wrapper.instance().generateCoin(coin);
+  const expected = <p className="coinValue change">{coin.toString()}<button className="coin bronze" onClick={ () => this.updateBalance(0.01) }></button></p>
+  expect(JSON.stringify(actual)).toEqual(JSON.stringify(expected));
+})
