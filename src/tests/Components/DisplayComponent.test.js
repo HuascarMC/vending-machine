747a96116ef336bda97bbe693753e6d901c8c26c
Huascar <huascarmc@hotmail.com>
2018-05-06 13:18:35 +0100

    should render machine-display div test passed

--- src/tests/Components/DisplayComponent.test.js
+++ src/tests/Components/DisplayComponent.test.js
@@ -4,5 +4,11 @@ import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
 
 it('should render without crashing', () => {
-  shallow(<DisplayComponent amount={0.00}/>);
+  shallow(<DisplayComponent value={0.00}/>);
+});
+
+it('should render machine-display div', () => {
+  const wrapper = shallow(<DisplayComponent value={0.00}/>);
+  const div = wrapper.find('.machine-display');
+  expect(div.exists()).toEqual(true);
 });
